7cef6122161f7120acadf4e79ba1f2790b414021
Mandy Fung <frozenera@hotmail.com>
2016-09-17 21:57:11 -0700

    Layout changes

--- src/com/example/cancerreport/Main.java
+++ src/com/example/cancerreport/Main.java
@@ -21,7 +21,7 @@ public class Main extends Activity {
 	 * Whether or not the system UI should be auto-hidden after
 	 * {@link #AUTO_HIDE_DELAY_MILLIS} milliseconds.
 	 */
-	private static final boolean AUTO_HIDE = true;
+	private static final boolean AUTO_HIDE = false;
 
 	/**
 	 * If {@link #AUTO_HIDE} is set, the number of milliseconds to wait after
@@ -33,7 +33,7 @@ public class Main extends Activity {
 	 * If set, will toggle the system UI visibility upon interaction. Otherwise,
 	 * will show the system UI visibility upon interaction.
 	 */
-	private static final boolean TOGGLE_ON_CLICK = true;
+	private static final boolean TOGGLE_ON_CLICK = false;
 
 	/**
 	 * The flags to pass to {@link SystemUiHider#getInstance}.
